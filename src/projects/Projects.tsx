eb2bdcdafdaa0b1692debc4d23b5b0cd85d46f23
RinalisDS <denispilyutin@gmail.com>
2022-05-10 01:28:29 +0300

    3rd project

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -4,6 +4,7 @@ import {Project} from "./Project/Project";
 import {Title} from '../common/components/title/Title';
 import socialImage from './../assets/image/social.jpg'
 import todolistImage from './../assets/image/todolist.jpg'
+import cardsImage from './../assets/image/cards.jpg'
 
 export const Projects: FC = () => {
   const socialNetworkStyle = {
@@ -11,6 +12,10 @@ export const Projects: FC = () => {
   }
   const todolistStyle = {
     backgroundImage: `url(${todolistImage})`
+  }
+
+  const cardsStyle = {
+    backgroundImage: `url(${cardsImage})`
   }
   return (
     <div className={styles.projectsBlock} id={'projects'}>
@@ -21,6 +26,9 @@ export const Projects: FC = () => {
                    description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus non obcaecati porro velit? Aliquam, culpa.'}/>
           <Project siteUrl={'https://rinalinds.github.io/socialnetwork'} style={socialNetworkStyle}
                    title={'SOCIAL NETWORK'}
+                   description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus non obcaecati porro velit? Aliquam, culpa.'}/>
+          <Project siteUrl={'https://theidiet.github.io/group_projest/#/'} style={cardsStyle}
+                   title={'Learning Cards'}
                    description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus non obcaecati porro velit? Aliquam, culpa.'}/>
         </div>
       </div>
