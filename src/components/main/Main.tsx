69715f815d8dd254cc33c9bb4d1c7f6e9b700710
RinalisDS <denispilyutin@gmail.com>
2022-05-10 19:30:48 +0300

    made fucking particles

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -3,12 +3,24 @@ import styles from './Main.module.scss';
 import {Fade} from 'react-awesome-reveal';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
+import {Engine, ISourceOptions} from 'tsparticles-engine';
+import {loadLinksPreset} from 'tsparticles-preset-links';
+import Particles from 'react-tsparticles';
 
 export function Main() {
+  const options:ISourceOptions = {
+    preset: "links",
+  };
+  const initialize =  async (engine: Engine) => {
+    await loadLinksPreset(engine);
+  };
   return (
     <Fade>
+
       <div className={styles.main} id={'main'}>
+
         <div className={styles.container}>
+          <Particles options={options} init={initialize} className={styles.particles} />
           <div className={styles.greetings}>
             <span>Hello there!</span>
             <span>My name is Pilyutin <span>Denis</span></span>
