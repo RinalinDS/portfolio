be6270093a89e279117074cf20f6f58939998f9e
DenisPilyutin <denispilyutin@gmail.com>
2023-11-14 00:42:37 +0200

    feat: add exp, delete contacts

--- src/components/header/constants/navLinks.ts
+++ src/components/header/constants/navLinks.ts
@@ -2,7 +2,7 @@ export enum Link {
   Bio = '#bio',
   Skills = '#skills',
   Projects = '#projects',
-  Contacts = '#contacts',
+  Education = '#education',
   Socials = '#socials',
 }
 
@@ -10,6 +10,6 @@ export const navLinks = [
   { id: 1, link: Link.Bio, label: 'Bio' },
   { id: 2, link: Link.Skills, label: 'Skills' },
   { id: 3, link: Link.Projects, label: 'Projects' },
-  { id: 4, link: Link.Contacts, label: 'Contacts' },
+  { id: 4, link: Link.Education, label: 'Education' },
   { id: 5, link: Link.Socials, label: 'Socials' },
 ];
