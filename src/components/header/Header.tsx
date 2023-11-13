49583aa6739dc97fdaabd8bb06d378a6fa5bfa96
DenisPilyutin <denispilyutin@gmail.com>
2023-11-13 15:13:27 +0200

    chore: delete react

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,11 @@
-import React, {FC} from 'react';
+import { FC } from 'react';
 import styles from './Header.module.scss';
-import {Nav} from "./nav/Nav";
+import { Nav } from "./nav/Nav";
 
-export const Header:FC =() => {
+export const Header: FC = () => {
     return (
         <div className={styles.header} id={'home'}>
-            <Nav/>
+            <Nav />
         </div>
     );
 }
