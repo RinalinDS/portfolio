a5ecd53f3fc820418577bcde657c0c6fa6ab6989
DenisPilyutin <denispilyutin@gmail.com>
2023-11-18 14:37:05 +0200

    feat: fitler projects

--- src/components/projects/contants/projects.ts
+++ src/components/projects/contants/projects.ts
@@ -1,15 +1,15 @@
-import socialImage from '../../../assets/image/projects/social.jpg';
-import todolistImage from '../../../assets/image/projects/todolist.jpg';
+// import socialImage from '../../../assets/image/projects/social.jpg';
+// import todolistImage from '../../../assets/image/projects/todolist.jpg';
 import cardsImage from '../../../assets/image/projects/cards.jpg';
 import weatherImage from '../../../assets/image/projects/weather.jpg';
 import trelloImage from '../../../assets/image/projects/trello.jpg';
 
 export enum Project {
   trello = 'Trello application',
-  todolist = 'Todolist application',
   weather = 'Weather application',
   cards = 'Learning cards',
-  socialNetwork = 'Social network',
+  // todolist = 'Todolist application',
+  // socialNetwork = 'Social network',
 }
 
 type ProjectDetails = {
@@ -79,30 +79,30 @@ const projectDetails: ProjectDetails = {
       backgroundImage: `url(${weatherImage})`,
     },
   },
-  [Project.todolist]: {
-    techStack:
-      ' Technologies: JavaScript, TypeScript, React, Redux, Redux-Thunk Material UI, SCSS, Formik',
-    description: `SPA for managing to-do lists and their items.
-Implemented: add todolist / task, remove todolist / task,
-rename todolist / task, marking the completed task, displaying all / active / confirmed tasks in todolist.`,
-    style: {
-      backgroundImage: `url(${todolistImage})`,
-    },
-  },
-  [Project.socialNetwork]: {
-    description:
-      'SPA for social actions - messaging and blogging. Implemented: ' +
-      'login page with validation, error displaying and captcha; 404 page;' +
-      ' profile page with abilities to: view the information of the selected user, edit your profile information, ' +
-      'change avatar, add a new post and view your published posts; ' +
-      'dialogs page with the ability to send a message; users page and friends page with abilities to: ' +
-      'view the list of users using pagination, subscribe and unsubscribe to any user, view any user profile.',
-    style: {
-      backgroundImage: `url(${socialImage})`,
-    },
-    techStack:
-      'Technologies: JavaScript, TypeScript, React, Redux, Redux-thunk, Redux-form, Axios',
-  },
+  //   [Project.todolist]: {
+  //     techStack:
+  //       ' Technologies: JavaScript, TypeScript, React, Redux, Redux-Thunk Material UI, SCSS, Formik',
+  //     description: `SPA for managing to-do lists and their items.
+  // Implemented: add todolist / task, remove todolist / task,
+  // rename todolist / task, marking the completed task, displaying all / active / confirmed tasks in todolist.`,
+  //     style: {
+  //       backgroundImage: `url(${todolistImage})`,
+  //     },
+  //   },
+  //   [Project.socialNetwork]: {
+  //     description:
+  //       'SPA for social actions - messaging and blogging. Implemented: ' +
+  //       'login page with validation, error displaying and captcha; 404 page;' +
+  //       ' profile page with abilities to: view the information of the selected user, edit your profile information, ' +
+  //       'change avatar, add a new post and view your published posts; ' +
+  //       'dialogs page with the ability to send a message; users page and friends page with abilities to: ' +
+  //       'view the list of users using pagination, subscribe and unsubscribe to any user, view any user profile.',
+  //     style: {
+  //       backgroundImage: `url(${socialImage})`,
+  //     },
+  //     techStack:
+  //       'Technologies: JavaScript, TypeScript, React, Redux, Redux-thunk, Redux-form, Axios',
+  //   },
 };
 
 export const projects: ProjectType[] = [
@@ -134,22 +134,22 @@ export const projects: ProjectType[] = [
     description: projectDetails[Project.cards].description,
     techStack: projectDetails[Project.cards].techStack,
   },
-  {
-    id: 1,
-    githubURL: 'https://github.com/RinalinDS/mytodolist',
-    siteURL: 'https://rinalinds.github.io/mytodolist',
-    style: projectDetails[Project.todolist].style,
-    title: Project.todolist,
-    description: projectDetails[Project.todolist].description,
-    techStack: projectDetails[Project.todolist].techStack,
-  },
-  {
-    id: 2,
-    githubURL: 'https://github.com/RinalinDS/socialnetwork',
-    siteURL: 'https://rinalinds.github.io/socialnetwork',
-    style: projectDetails[Project.socialNetwork].style,
-    title: Project.socialNetwork,
-    description: projectDetails[Project.socialNetwork].description,
-    techStack: projectDetails[Project.socialNetwork].techStack,
-  },
+  // {
+  //   id: 1,
+  //   githubURL: 'https://github.com/RinalinDS/mytodolist',
+  //   siteURL: 'https://rinalinds.github.io/mytodolist',
+  //   style: projectDetails[Project.todolist].style,
+  //   title: Project.todolist,
+  //   description: projectDetails[Project.todolist].description,
+  //   techStack: projectDetails[Project.todolist].techStack,
+  // },
+  // {
+  //   id: 2,
+  //   githubURL: 'https://github.com/RinalinDS/socialnetwork',
+  //   siteURL: 'https://rinalinds.github.io/socialnetwork',
+  //   style: projectDetails[Project.socialNetwork].style,
+  //   title: Project.socialNetwork,
+  //   description: projectDetails[Project.socialNetwork].description,
+  //   techStack: projectDetails[Project.socialNetwork].techStack,
+  // },
 ];
